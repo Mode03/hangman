37ebe2759692316d7e5385ef960bf9a2f9f5dbad
Mode03 <supermode2003@gmail.com>
2026-08-14 21:18:44 +0300

    feat: only display correctly guessed letters

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export default function AssemblyEndgame() {
   const [currentWord, setCurrentWord] = useState("react")
   const [guessedLetters, setGuessedLetters] = useState([])
 
-  console.log(guessedLetters)
-  
+  const wrongGuessCount = guessedLetters.filter(letter => !currentWord.includes(letter)).length
+
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
   function addGuessedLetter(letter) {
@@ -33,7 +33,9 @@ export default function AssemblyEndgame() {
   })
   
   const letterElements = currentWord.split("").map((letter, index) => (
-    <span key={index}>{letter.toUpperCase()}</span>
+    <span key={index}>
+      {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+    </span>
   ))
   
   const keyboardElements = alphabet.split("").map(letter => {
